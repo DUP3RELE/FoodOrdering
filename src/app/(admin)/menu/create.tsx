dc3094826e12a6587d4f3594443c91e74345837e
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-07-30 09:31:48 +0200

    product creation

---
+++ src/app/(admin)/menu/create.tsx
@@ -0,0 +1,64 @@
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import Button from "@/components/Button";
+
+const CreateProductScreen = () => {
+	const [name, setName] = useState("");
+	const [price, setPrice] = useState("");
+
+	const resetFields = () => {
+		setName("");
+		setPrice("");
+	};
+
+	const onCreate = () => {
+		console.warn("Creating product", name);
+        // save in database
+		resetFields();
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.label}>Name</Text>
+			<TextInput
+				value={name}
+				onChangeText={setName}
+				placeholder='Name'
+				style={styles.input}
+			/>
+			<Text style={styles.label}>Price ($)</Text>
+			<TextInput
+				value={price}
+				placeholder='9.99'
+				style={styles.input}
+				keyboardType='numeric'
+				onChangeText={setPrice}
+			/>
+
+			<Button
+				onPress={onCreate}
+				text='Create'
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		padding: 10,
+	},
+	input: {
+		backgroundColor: "white",
+		padding: 10,
+		borderRadius: 5,
+		marginTop: 5,
+		marginBottom: 20,
+	},
+	label: {
+		color: "gray",
+		fontSize: 16,
+	},
+});
+export default CreateProductScreen;
