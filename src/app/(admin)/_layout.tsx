0e32f037bdc5a36ebebbed8e552525c47319430d
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-08-05 16:35:51 +0200

    admin side orders

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -51,9 +51,9 @@ export default function TabLayout() {
 				}}
 			/>
 			<Tabs.Screen
-				name='two'
+				name='orders'
 				options={{
-					title: "Two",
+					title: "Orders",
 					headerShown: false,
 					tabBarIcon: ({ color }) => (
 						<TabBarIcon
