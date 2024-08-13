9308e3b1470d8953d8322b8fae1aa13fb63558bd
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-08-13 20:46:26 +0200

    reading orders

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,18 +1,21 @@
-import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
+import { View, Text, StyleSheet, FlatList, Pressable, ActivityIndicator } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
 import orders from "../../../../assets/data/orders";
 import OrderItemListItem from "@/components/OrderItemListItem";
 import OrderListItem from "../../../components/OrderListItem";
 import Colors from "@/constants/Colors";
 import { OrderStatusList } from "@/types";
+import { useOrderDetails } from "@/api/orders";
 
 const OrderDetailScreen = () => {
-	const { id } = useLocalSearchParams();
-
-	const order = orders.find((o) => o.id.toString() === id);
-
-	if (!order) {
-		return <Text>Order not found!</Text>;
+	const { id: idString } = useLocalSearchParams();
+	const id = parseFloat(typeof idString == "string" ? idString : idString[0]);
+	const { data: order, error, isLoading } = useOrderDetails(id);
+	if (isLoading) {
+		return <ActivityIndicator />;
+	}
+	if (error) {
+		return <Text>Failed to fetch</Text>;
 	}
 
 	return (
